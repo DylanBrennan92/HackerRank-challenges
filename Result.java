07e7732a64d013ba566b4f38d383e9e591313241
DylanBrennan92 <x18119859@student.ncirl.ie>
2022-06-06 13:43:56 +0100

    challenge 3

---
+++ Result.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import java.util.HashMap;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+public class Result {
+    public static String timeConversion(String s){
+        
+        
+
+
+
+        return s;
+    }
+}
